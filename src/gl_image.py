cf91ec5b7fe4a19fa3ca737d63ca255f972d0981
Jesse <jgodwin1420@gmail.com>
2022-10-06 21:36:18 +0100

    added identicons

--- src/gl_image.py
+++ src/gl_image.py
@@ -1,13 +1,14 @@
 import PIL.Image
 from argparse import ArgumentParser
-
+from github.identicon import Identicon
+from random import randint
 
 GIT_LEVELS = ["0", "1", "2", "3", "4" ]
 # 0, 3, 8, 10, 13
 
 def resize_image(image):
-    # convert image into 52x7 size
-    resized_image = image.resize((52, 7))
+    # convert image into 7x7 size
+    resized_image = image.resize((7, 7))
     return resized_image
 
 def grayify(image):
@@ -17,36 +18,23 @@ def grayify(image):
     
 def convert_to_gl(image):
     pixels = image.getdata()
-    characters = "".join([GIT_LEVELS[pixel//51] for pixel in pixels])
+    characters = "".join([GIT_LEVELS[pixel//52] for pixel in pixels])
     return characters
 
-def main(path, new_width=52):
-    # ascii characters used to build the output text
-
-
-    # read image
-    try:
-        img = PIL.Image.open(path)
-    except:
-        print(path, "is not valid")
-
-    new_image_data = convert_to_gl(grayify(resize_image(img)))
+def identicon(new_width=7):
+    identicon = Identicon.from_identifier(randint(0, 480938))
+    image = identicon.generate_image()  # requires PIL
+    new_image_data = convert_to_gl(grayify(resize_image(image)))
 
     pixel_count = len(new_image_data)
     gl_image = "\n".join([new_image_data[index:(index+new_width)] for index in range(0, pixel_count, new_width)])
 
-    print(gl_image)
+    return gl_image
 
-    with open("gl_image.txt", "w") as f:
-        f.write(gl_image)
 
+    
 
 
 if __name__ == '__main__':
-    # image file as command line argument
-    parser = ArgumentParser(description="Convert an image to a git level image")
-    parser.add_argument("path", help="The path to the image file")
-    args = parser.parse_args()
-
-    main(path=args.path)
+    print(identicon())
 
--- src/script.py
+++ src/script.py
@@ -2,6 +2,8 @@ import subprocess
 import time 
 from datetime import datetime
 import random
+from gl_image import identicon
+
 def commit():
     file = open("dummy.txt", "a")
     file.write(" ".join([random.choice(["This", "is", "a", "work", "of", "Art"]) for i in range(0, 10)]))
@@ -11,23 +13,22 @@ def commit():
 
 def main():
     GIT_DICT = {
-        "0": 5,
+        "0": 0,
         "1": 3,
         "2": 8,
         "3": 10,
         "4": 13
     }
 
-    while True:
-        gl_image = open("gl_image.txt", "r").read().replace("\n", "")
-        i = 0 
+    gl_image = identicon().replace("\n", "")
+
+    for i in range(len(gl_image) - 1):
         current_time = datetime.now().strftime("%H:%M:%S")
-        print(current_time)
+        print(f"{current_time} - {i} - {gl_image[i]} - {GIT_DICT[gl_image[i]]}")
         if current_time == "20:26:00":
-            i += 1
             for _ in range(0, GIT_DICT[gl_image[i]]):
                 commit()
-                
+        
 
 if __name__ == '__main__':
     main()
